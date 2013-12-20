d350f264fab70b472d22d43dc73a05312330c5b0
hehao <mickeymousesysu@gmail.com>
2013-12-21 03:56:52 +0800

    modify main.php,add log,cache,urlManager,mysqlDB connfig

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,9 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'中山大学多媒体信息服务平台',
+	'homeUrl'=>'?r=site/index',
+	'language'=>'zh_cn',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -20,14 +22,13 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'123456',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		
 	),
 
 	// application components
@@ -37,7 +38,7 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -46,20 +47,22 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+		*/
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=mis',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => '123',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -67,10 +70,38 @@ return array(
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
-				array(
+				array( 		//数据库日志记载在db.log中
 					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
+					'categories'=>'db.*',
+					'logFile'=>'db.log',
 				),
+				array(		//用户交互的日志记载在uc.log中
+			   		'class'=>'CFileLogRoute',
+					'categories'=>'uc.*',
+					'logFile'=>'uc.log',
+				),
+				array(		//error日志单独保存到error.log中
+			   		'class'=>'CFileLogRoute',
+					'levels'=>'error',
+					'logFile'=>'error.log',
+				),
+				array(		//其他日志保存到warning.log中
+			   		'class'=>'CFileLogRoute',
+					'levels'=>'trace, info, profile, warning',
+					'logFile'=>'warning.log',
+				),
+			 	array(		//用户中心的错误日志直接发邮件
+			   		'class'=>'CEmailLogRoute',
+					'categories'=>'uc.*',
+					'levels'=>'error',
+					'emails'=>'hehao5@mail2.sysu.edu.cn',
+				),	
+				array(		//开发过程中，所有日志直接打印到页面
+
+			   		'class'=>'CWebLogRoute',
+					'levels'=>'trace, info, profile, warning, error',
+				),
+	
 				// uncomment the following to show log messages on web pages
 				/*
 				array(
@@ -79,12 +110,26 @@ return array(
 				*/
 			),
 		),
+		'request'=>array(	//防范cookie欺骗和csrf攻击
+		      	'enableCsrfValidation'=>true,
+			'enableCookieValidation'=>true,      
+		),
+		/*
+		'cache'=>array(		//memcache缓存配置，暂不启用，后期性能调优时再追加
+			'class'=>'system.caching.CMemCache',
+			'servers'=>array(
+			 	array('host'=>'server1', 'port'=>11211, 'weight'=>60),  
+			 	array('host'=>'server2', 'port'=>11211, 'weight'=>40),  
+			 ),
+		),
+		*/
+		
 	),
 
 	// application-level parameters that can be accessed
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'hehao5@mail2.sysu.edu.cn',
 	),
 );
