87bf747a0e87e4607abd977af0ef436b8f675b44
hehao <mickeymousesysu@gmail.com>
2014-02-20 18:57:43 +0800

    add MMC Authority bit

--- protected/components/ModuleAuth.php
+++ protected/components/ModuleAuth.php
@@ -9,16 +9,24 @@
 
 interface ModuleAuth
 {
-   const AUTH_SUPER_ADMIN = 0;
-   const AUTH_HOMEPAGE_ADMIN = 2;
-   const AUTH_SPECIAL_CLASSROOM = 4;
-   const AUTH_SERVICE_ADMIN = 8;
-   const AUTH_RULE_ADMIN = 16;
-   const AUTH_TECH_EXPLORE = 32;
-   const AUTH_TEAMSTYLE = 64;
-   const AUTH_COMMENT_ADMIN = 128;
-   const AUTH_USER_ADMIN = 256;
-   const AUTH_TRASH_ADMIN = 512;
+   /*  mmc  authority bit 0~8 bits used */
+   const MMC_SUPER_ADMIN = 1;
+   const MMC_HOMEPAGE_ADMIN = 2;
+   const MMC_SPECIAL_CLASSROOM = 4;
+   const MMC_SERVICE_ADMIN = 8;
+   const MMC_RULE_ADMIN = 16;
+   const MMC_TECH_EXPLORE = 32;
+   const MMC_TEAMSTYLE = 64;
+   const MMC_COMMENT_ADMIN = 128;
+   const MMC_USER_ADMIN = 256;
+   const MMC_TRASH_ADMIN = 512;
+
+   /* mmd authority bit  16 17 18 19  used*/
+
+   const MMD_DEVICE_CHECK =  65536;
+   const MMD_DEVICE_INFO = 131072;
+   const MMD_ACTION_MANAGE = 262144;
+   const MMD_SERVICE_SURVEY = 524288;
 }
 
 
