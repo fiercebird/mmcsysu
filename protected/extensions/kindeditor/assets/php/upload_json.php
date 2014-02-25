7a95655471592656f2d0c6810e2751ac12792f17
hehao <mickeymousesysu@gmail.com>
2014-02-26 00:19:10 +0800

    alter upload file type

--- protected/extensions/kindeditor/assets/php/upload_json.php
+++ protected/extensions/kindeditor/assets/php/upload_json.php
@@ -17,12 +17,12 @@ $save_path = $php_path . '../attached/';
 $save_url = $php_url . '../attached/';
 //定义允许上传的文件扩展名
 $ext_arr = array(
-	//'image' => array('gif', 'jpg', 'jpeg', 'png', 'bmp'),
-	//'flash' => array('swf', 'flv'),
-	//'media' => array('swf', 'flv', 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb'),
-	//'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2'),
-	'image' => array('jpg', 'png'),
-	'file' => array('zip', 'rar'),
+	'image' => array('gif', 'jpg', 'jpeg', 'png', 'bmp'),
+	'flash' => array('swf', 'flv'),
+	'media' => array('swf', 'flv', 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb'),
+	'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2'),
+//	'image' => array('jpg', 'png'),
+//	'file' => array('zip', 'rar'),
 );
 //最大文件大小2MB
 $max_size = 2097152;
